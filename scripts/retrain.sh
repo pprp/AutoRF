6aae9872847a56285109df933f123f177e172aa4
pprp <1115957667@qq.com>
2022-01-25 22:05:39 +0800

    update retrain sh

--- scripts/retrain.sh
+++ scripts/retrain.sh
@@ -3,9 +3,39 @@
 # python tools/retrain.py --arch RFSTEP3 --cutout --cutout_length 8 --model_name "retain_rf_resnet_cutout_8"
 
 
-# python tools/retrain.py --arch Attention  --model_name "autola_base"
+# python tools/retrain.py --model_base 'resnet20' --model_name 'resnet20_base' --arch 'NORMAL'
 
-python tools/retrain.py --model_base 'resnet20' --model_name 'resnet20_base' --arch 'NORMAL'
+# python tools/retrain.py --model_base 'resnet32' --model_name 'resnet32_base' --arch 'NORMAL'
+
+# python tools/retrain.py --model_base 'resnet44' --model_name 'resnet44_base' --arch 'NORMAL'
+
+# python tools/retrain.py --model_base 'resnet56' --model_name 'resnet56_base' --arch 'NORMAL'
+
+# python tools/retrain.py --model_base 'resnet110' --model_name 'resnet110_base' --arch 'NORMAL'
+
+# python tools/retrain.py --model_base 'rf_resnet20' --model_name 'rf_resnet20_base_rfstep3' --arch RFSTEP3
+
+# python tools/retrain.py --model_base 'rf_resnet32' --model_name 'rf_resnet20_base_rfstep3' --arch RFSTEP3
+
+# python tools/retrain.py --model_base 'rf_resnet44' --model_name 'rf_resnet44_base_rfstep3' --arch RFSTEP3
+
+# python tools/retrain.py --model_base 'rf_resnet56' --model_name 'rf_resnet56_base_rfstep3' --arch RFSTEP3
+
+# python tools/retrain.py --model_base 'rf_resnet110' --model_name 'rf_resnet110_base_rfstep3' --arch RFSTEP3
+
+# python tools/retrain.py --model_base 'la_resnet20' --model_name 'la_resnet20_base_rfstep3' --arch Attention
+
+# python tools/retrain.py --model_base 'la_resnet32' --model_name 'la_resnet20_base_rfstep3' --arch Attention
+
+# python tools/retrain.py --model_base 'la_resnet44' --model_name 'la_resnet44_base_rfstep3' --arch Attention
+
+python tools/retrain.py --model_base 'la_resnet56' --model_name 'la_resnet56_base_rfstep3' --arch Attention
+
+# python tools/retrain.py --model_base 'la_resnet110' --model_name 'la_resnet110_base_rfstep3' --arch Attention
+
+
+
+
 
 # module load  cuda/10.1
 # module load cudnn/7.6.5.32_cuda10.1u2
